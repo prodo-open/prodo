958e9fb0873d88a0aa730edad2fbae29d45ccd9c
Ali Zahid <ali.zahid@live.com>
2016-02-17 18:48:53 +0500

    Bootstrapped the project
    - Prodo logo added
    - Avenir Next added
    - Styles added
    - Layout added
    - SASS and Autoprefixer added
    - Refactoring

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express'),
+	path = require('path'),
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	sass = require('node-sass-middleware'),
+	autoprefixer = require('express-autoprefixer');
 
 var app = express();
+
+// sass
+
+app.use(sass({
+	src: path.join(__dirname, 'public'),
+	outputStyle: 'compressed'
+}));
+app.use(autoprefixer({
+	browsers: 'last 2 versions',
+	cascade: false
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -23,6 +32,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+
+var routes = require('./routes/index'),
+	users = require('./routes/users');
 
 app.use('/', routes);
 app.use('/users', users);
